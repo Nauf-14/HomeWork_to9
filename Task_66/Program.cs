fd4165742aca2e067ad588ad90937d1f7b062d06
Naufal <nauf1207@mail.ru>
2023-04-05 14:37:49 +0600

    Сумму натуральных элементов в промежутке от M до N.Рекурсия

---
+++ Task_66/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задайте значения M и N. Напишите программу,
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+int f(int a, int b)
+{
+    if (a > b)
+        return 0;
+    return a + f(a + 1, b);
+    
+}
+Console.Clear();
+Console.Write("Введите начальное число: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите конечное число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine(f(m, n));
